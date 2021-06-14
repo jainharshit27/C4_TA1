f99f46f830cbe3092f403f99fe92d7d018fe9a6c
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-06-14 15:43:08 +0530

    Add files via upload

---
+++ C4 TA1.py
@@ -0,0 +1,28 @@
+#Arithmetic Operators
+a = 12
+b = 2.4
+
+print(a + b)
+print(a / b)
+print(a ** b)
+
+
+
+#Comparison Operators
+a = 12
+b = 2.4
+
+print(a <= b)
+print(a != b)
+print(a > b)
+
+
+
+#Assignment Operators
+x = 5
+x = x + 3
+print(x)
+# Same can be written as:
+x = 5
+x += 3
+print(x)
